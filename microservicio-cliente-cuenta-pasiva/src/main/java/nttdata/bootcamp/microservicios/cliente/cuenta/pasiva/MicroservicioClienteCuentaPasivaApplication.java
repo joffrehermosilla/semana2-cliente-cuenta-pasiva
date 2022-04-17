ae5df7321acc8d49921c5c0c6425d7e3768d3ce0
Joffre Hermosilla Salas <jhermoss@emeal.nttdata.com>
2022-04-17 13:30:44 -0500

    primer commit

---
+++ microservicio-cliente-cuenta-pasiva/src/main/java/nttdata/bootcamp/microservicios/cliente/cuenta/pasiva/MicroservicioClienteCuentaPasivaApplication.java
@@ -0,0 +1,13 @@
+package nttdata.bootcamp.microservicios.cliente.cuenta.pasiva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioClienteCuentaPasivaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioClienteCuentaPasivaApplication.class, args);
+	}
+
+}
---
+++ microservicio-cliente-cuenta-pasiva/src/test/java/nttdata/bootcamp/microservicios/cliente/cuenta/pasiva/MicroservicioClienteCuentaPasivaApplicationTests.java
@@ -0,0 +1,13 @@
+package nttdata.bootcamp.microservicios.cliente.cuenta.pasiva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioClienteCuentaPasivaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
